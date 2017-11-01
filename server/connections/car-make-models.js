1c6e81aaa550c995a78613215b3fa476dca200c1
Kevin Downey <Kevin_Downey@intuit.com>
2017-11-01 14:57:57 -0700

    adding new carmakemodel type

---
+++ server/connections/car-make-models.js
@@ -0,0 +1,24 @@
+import { GraphQLInt, GraphQLFloat } from 'graphql';
+import { connectionDefinitions } from 'graphql-relay';
+
+import { carType } from '../types/car-type';
+
+export const {
+  connectionType: carConnectionType,
+  edgeType: carEdgeType
+} = connectionDefinitions({
+  
+  name: 'Cars',
+  nodeType: carType,
+
+  connectionFields: () => ({
+    totalCount: {
+      type: GraphQLInt,
+    },
+    totalPrice: {
+      type: GraphQLFloat,
+    },
+
+  }),
+
+});
---
+++ server/models/car-make-model-data.js
@@ -0,0 +1,9 @@
+import { BaseData } from './base-data';
+
+export class CarMakeModelData extends BaseData {
+
+  constructor(baseUrl) {
+    super(baseUrl, 'carmakemodels');
+  }
+
+}
